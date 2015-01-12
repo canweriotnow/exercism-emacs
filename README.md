e00b4a5951181be5c86636683fc71dc51a2207c9
Jason Lewis <jason@decomplecting.org>
2015-01-12 16:04:00 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,51 @@
+#exercism.el
+
+An Emacs package for [exercism.io](http://exercism.io).
+
+Currently provides two commands, which sort of work some of the time:
+
+From an exercism exercise buffer,
+
+`M-x exercism-submit`
+
+will submit the current exercise.
+
+From the context of your exercism directory tree,
+
+`M-x exercism-fetch` should fetch any new exercises if they are available.
+
+## Installation
+
+Put `exercism.el` in one of your personal module autoload directories.
+
+I'm not pushing to [MELPA](http://melpa.milkbox.net) until this is stable.
+
+## TODO
+
+exercism.el currently executes the exercism shell commands, just to get an initial package together so my lazy ass doesn't have to leave emacs to submit/fetch exercism exercises
+
+The long-range goal is to port most if not all of the [Exercism CLI](https://github.com/exercism/cli) to elisp.
+
+Once that's done, hooking into unit testing for some of the exercism language tracks would be nice.
+
+## Contributing
+
+YES, PLEASE. This is my first attempt at an emacs package, so any pull requests, suggestions, issues, etc. would be greatly appreciated.
+
+
+## License
+
+Copyright (C) 2015  Jason Lewis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ exercism.el
@@ -0,0 +1,91 @@
+;;; exercism.el - interact with exercism.io from within emacs
+
+;; Author: Jason Lewis
+;; URL:
+;; Created 2015.01.12
+;; Version: 0.0.1
+
+;; This file is not part of GNU Emacs
+
+;; Copyright (C) 2015  Jason Lewis
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Exercism mode provides functions for interacting with exercises from
+;; exercism.io, submitting completed exercises, and fetching new ones.
+;; It aims to eventually integrate with exercism unit tests and (possibly?)
+;; auto-submit work when all tests pass.
+
+;; The goal is to port the exercism CLI client from Go to elisp, but I think
+;; for this first iteration I might just try to shell out to the CLI to run
+;; submit and fetch.
+
+;;; Code:
+
+(require 'cl)
+(require 'url)
+(require 'json)
+
+;;;###autoload
+(progn
+  (defgroup exercism nil
+    "Exercism.io"
+    :group 'external)
+
+  (defcustom exercism-api-key ""
+    "API key found on the /account page of exercism"
+    :group 'exercism
+    :type 'string))
+
+(defmacro namespace (ns-name symbols-to-namespace &rest body)
+  "Local alias symbols that expand to namespace"
+  `(let) body)
+
+(defconst exercism-base-url "http://exercism.io"
+  "endpoint to submit solutions to, and to get personalized data")
+
+(defconst exercism-fetch-url "http://x.exercism.io"
+  "endpoint to fetch problems from")
+
+(defvar *exercism-current-exercise*)
+
+
+
+;;;;; Interactive User Funs
+
+;;;###autoload
+(defun exercism-submit ()
+  (interactive)
+  (let ((exercise buffer-file-name)
+          (cmd (shell-command-to-string "which exercism")))
+      (if (zerop (length cmd))
+          "exercism not found"
+        (shell-command-to-string (concat "exercism submit " exercise)))))
+
+;;;###autoload
+(defun exercism-fetch ()
+  (interactive)
+  (let ((cmd (shell-command-to-string "which exercism")))
+    (if (zerop (length cmd))
+        "exercism not found"
+      (shell-command-to-string (concat cmd " fetch")))))
+
+
+
+
+(provide 'exercism)
+
+;;; exercism.el ends here
