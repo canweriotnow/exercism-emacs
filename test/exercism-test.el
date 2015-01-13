12587e82094b1efd514333915f574ba8cc44d396
Jason Lewis <jason@decomplecting.org>
2015-01-13 15:11:11 -0500

    Adding testing instrumentation as per #8

---
+++ test/exercism-test.el
@@ -0,0 +1,16 @@
+;;; exercism-test --- test suite for exercism.el
+
+;;; Commentary:
+
+;;; Code:
+
+(load-file "exercism.el")
+
+(ert-deftest is-true ()
+  (eq t nil))
+
+
+
+(provide 'exercism-test)
+
+;;; exercism-test.el ends here
