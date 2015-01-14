b99dca289b6f82223cdae3dde11442e069a92bd5
Jason Lewis <jason@decomplecting.org>
2015-01-13 22:18:45 -0500

    Starting to test actual functions... probably poorly.
    Added test-exercism-get-config, just asserts the presence of the
    keys in the config plist and that their values are strings.

--- test/exercism-test.el
+++ test/exercism-test.el
@@ -6,12 +6,19 @@
 
 (load-file "exercism.el")
 
-
+;; Dummy
 (ert-deftest is-true ()
   (should (eq t t)))
 
 (ert-deftest is-nil ()
   (should (eq '() nil)))
+
+(ert-deftest test-exercism-get-config ()
+  (let ((config (exercism-get-config exercism-config-file)))
+    (should (stringp (plist-get config :api)))
+    (should (stringp (plist-get config :xapi)))
+    (should (stringp (plist-get config :apiKey)))
+    (should (stringp (plist-get config :dir)))))
 
 
 (provide 'exercism-test)
